57e4679795d248a84383fabe80db8abdd60aa09c
arapaill <57451389+arapaill@users.noreply.github.com>
2020-03-17 11:09:59 +0100

    lbft and gnl added
    ajout du Makefile, libft, GNL et du .h

---
+++ ft_cub3d.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_cub3d.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/17 10:34:11 by marvin            #+#    #+#             */
+/*   Updated: 2020/03/17 10:34:11 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_CUB3D_H
+# define FT_CUB3D_H
+
+# include <cmath>
+# include <string>
+# include <vector>
+# include <iostream>
+
+# include "get_next_line/libft/libft.h"
+# include "get_next_line/get_next_line.h"
+
+#endif
